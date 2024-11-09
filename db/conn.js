b77a133fd93ee07521632186febee62be5cf2a32
Vitor Souza <vitorugo_kta@hotmail.com>
2024-11-09 18:55:15 -0300

    corrijindo bd

--- db/conn.js
+++ db/conn.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');                        //Importando a biblioteca mysql2          
 const {Sequelize} = require('sequelize');               //Importando o sequelize
 //const senha = require('../oculto/senha');               //Importando o arquivo senha
-const senha = ''; 
+//const senha = ''; 
 
-const sequelize = new Sequelize('novo_clube', 'pic', '', {host: 'localhost', dialect: 'mysql'});
+const sequelize = new Sequelize('novo_clube', 'pic', '', {host: '127.0.0.1', dialect: 'mysql'});
 
 try {
     sequelize.authenticate();
