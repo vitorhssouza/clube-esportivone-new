276dc5a9c0ca92eb90bbd3360e11e092c7359855
Vitor Souza <vitorugo_kta@hotmail.com>
2024-11-15 18:39:37 -0300

    corrigindo conexão ao mysql do railway

--- db/conn.js
+++ db/conn.js
@@ -3,6 +3,7 @@ const {Sequelize} = require('sequelize');               //Importando o sequelize
 //const senha = require('../oculto/senha');               //Importando o arquivo senha
 //const senha = ''; 
 
+/*
 const sequelize = new Sequelize('novo_clube', 'pic', '', {host: '127.0.0.1', dialect: 'mysql'});
 
 try {
@@ -12,4 +13,31 @@ try {
     console.log('Error ao conectar com o DB' + error)
 }
 
+module.exports = sequelize;  */
+
+
+require('dotenv').config();
+// const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+  DB_NAME,
+  DB_USER,
+  DB_PASS,
+  {
+    host: DB_HOST,
+    dialect: 'mysql',
+    port: DB_PORT,
+  }
+);
+
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexão com o banco de dados foi estabelecida com sucesso.');
+  })
+  .catch(err => {
+    console.error('Não foi possível conectar ao banco de dados:', err);
+  });
+
 module.exports = sequelize;
+
